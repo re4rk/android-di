38ac191c44a036a233fd89dc55e9e8964175f286
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-08 16:04:22 +0900

    refactor : 테스트 모듈 삭제 후 실제 모듈에 적용

--- app/src/main/java/woowacourse/shopping/di/DiApplicationModule.kt
+++ app/src/main/java/woowacourse/shopping/di/DiApplicationModule.kt
@@ -1,18 +1,16 @@
 package woowacourse.shopping.di
 
 import com.re4rk.arkdi.DiContainer
+import com.re4rk.arkdi.Provides
 import woowacourse.shopping.data.CartSampleRepository
 import woowacourse.shopping.data.ProductSampleRepository
 import woowacourse.shopping.repository.CartRepository
 import woowacourse.shopping.repository.ProductRepository
 
-class DiApplicationModule : DiContainer() {
+object DiApplicationModule : DiContainer() {
+    @Provides
+    fun provideProductRepository(): ProductRepository = ProductSampleRepository()
 
-    val provideProductRepository: ProductRepository by lazy {
-        this.createInstance(ProductSampleRepository::class)
-    }
-
-    val provideCartRepository: CartRepository by lazy {
-        this.createInstance(CartSampleRepository::class)
-    }
+    @Provides
+    fun provideCartRepository(): CartRepository = CartSampleRepository()
 }
